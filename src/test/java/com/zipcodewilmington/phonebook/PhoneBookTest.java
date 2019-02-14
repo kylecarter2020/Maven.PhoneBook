bf7320a81430d0c3717d7477cd081f9b3ea35c59
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-14 17:51:49 -0500

    added junit and stubs for unit tests

--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,130 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void testHasEntryTrue(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testHasEntryFalse(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testAdd(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testAddMultipleNumbers(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testGetArrayListFor(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testLookup(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testLookupNumbersFor(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testReverseLookup(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testRemove(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testListNamesAndNumbers(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testListNames(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
+    @Test
+    public void testRemoveRecord(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        //When
+
+        //Then
+    }
+
 }
